1d6cdb7331438b764fec2d3744c750af3dcf4e6d
s473561 <wikgrz12@st.amu.edu.pl>
2021-12-23 13:57:13 +0100

    updated

---
+++ Makefile
@@ -0,0 +1,4 @@
+make:
+	gcc -ansi -Wall -o Microshell Microshell.c 
+clean:
+	rm -f Microshell.o 
--- Microshell.c
+++ Microshell.c
@@ -1,7 +1,57 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void getCurrentDirectory();
+void getLogin();
+void printPrompt();
+void command_help();
+
+char path[512], USER[512];
+char command[1024];
 
 int main()
 {
-    printf("ALA ma kota\n");
+
+
+   while (strcmp(command,"exit")!=0){
+    printPrompt();
+    scanf("%s", command);
+
+    if (strcmp(command, "help")==0){
+       command_help();
+    }else {
+
+    }
+
+   }
+
     return 0;
+}
+
+
+/*Commands*/
+void command_help()
+{
+    printf("Wiktoria Grzesik - Projekt Microshell\n");
+    printf("Available commands: exit\n");
+}
+
+/*Prompt*/
+void getCurrentDirectory()
+{
+    getcwd(path,sizeof(path));
+    printf("%s]\n$ ",path);
+}
+
+void getLogin()
+{
+    printf("[%s : ", getenv("USER"));
+}
+
+void printPrompt()
+{
+    getLogin();
+    getCurrentDirectory();
 }
